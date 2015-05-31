2f4730c44d78b6b937b8920089cd8f52f158ba25
Anshul Sahni <anshul_sahni@rediffmail.com>
2015-05-31 18:45:55 +0530

    completed one phase of web resume

---
+++ index.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Anshul Sahni</title>
+		<!-- website icon -->
+		<link rel="icon" href="https://graph.facebook.com/anshul.sahni.52/picture" size="32x32">
+
+		<!-- preparing support for bootstrap framework -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+		<script type="text/javascript" src="./js/jquery.min.js"></script>
+		<script type="text/javascript" src="./js/bootstrap.min.js"></script>
+		<!--  -->
+
+		<!-- Css files -->
+		<link rel="stylesheet" type="text/css" href="./css/index.css">
+		<!--  -->
+
+	</head>
+	<body>
+		<nav class='navbar navbar-inverse navbar-fixed-top'>
+		<div class='container-fluid'>
+			<div class='nav-header'>
+				<a class='navbar-brand' href="#">
+					<img src="./imgs/dp.jpg" class='img-circle' alt="Anshul Sahni" id='dp-img-small'>
+				</a>
+				<ul class='nav navbar-nav'>
+					<li class='active'><a href="aboutme">About Me</a>		</li>
+					<li>	<a href="#works">	Works			</a>	</li>
+					<li>	<a href="#academics">	Academics		</a>	</li>
+					<li>	<a href="#skills">	Skills			</a>	</li>
+					<li>	<a href="#contact">	Contact Me			</a>	</li>
+				</ul>
+			</div>
+		</div>
+		</nav>
+		<div class='container'>
+			<div class='row' id='aboutme'>
+				<div class='page-header'>
+					<h1>About Me...</h1>
+				</div>
+				<div class='col-md-1'></div>
+				<div class='col-md-3'>
+					<img src= "./imgs/dp.jpg" class='img-circle img-responsive' alt="Anshul Sahni" id='dp-img'>
+					<br><br>
+					<div class='well well-sm'>
+						<p class='text-center'>
+							<span class='glyphicon glyphicon-user'></span>&nbsp;&nbsp;Anshul Sahni <br>
+							<span class='glyphicon glyphicon-phone'></span>9840961451 <br>
+							<a href="mailto:anshul_sahni@live.com"><span class='glyphicon glyphicon-envelope'></span>&nbsp;&nbsp;anshul_sahni@live.com</a>
+						</p>
+						
+					</div>
+				</div>
+				<div class='col-md-7'>
+					<div class='panel panel-primary'>
+						<div class='panel-heading'>
+							About Me
+						</div>
+						<div class='panel-body'>
+							Hello readers, my name is Anshul Sahni, I am currently studying B.Tech in SRM University, I am a programmer, web developer, server administrator and I also love to design database structure for applications in short I am a computer geek who likes to explore technology and develops applications to ease the life of people.
+						</div>
+					</div>
+				</div>
+				<div class='col-md-1'></div>
+			</div>
+			<br><br><br><br>
+			<div class='row' id='works'>
+				<div class='page-header'>
+					<h1>My Works</h1>
+				</div>
+				<div class='col-md-1'></div>
+				<div class='col-md-10'>
+					<div class='panel panel-primary'>
+						<div class='panel-heading'>HP-Education Service</div>
+					</div>
+					<div>
+						<img src="./imgs/hp.png" class='img-circle work_icons pull-left'>
+						Underwent a four week training at <strong>HP Education Services, Noida</strong> and gained knowledge on subject of <strong>Advance Concept of Networking</strong>. Also completed a project titled <strong>Corporate Network with Advance Routing, Switching and Security</strong> designed on <strong>Networking Technology</strong>.
+					</div>
+					<br>
+					<div class='panel panel-primary'>
+						<div class='panel-heading'>WEBARCH</div>
+					</div>
+					<div> 
+						<img src="./imgs/webarch.png" class='img-circle work_icons pull-left'>
+						<p style='margin-left:60px;'>
+							Currently working as <strong>Joint Secretary in WEBARCH</strong>, which is a student run <strong>development and designing club</strong> of SRM University, joined this club in April 2013 and since then has worked upon the <strong>design and devlopment of many websites</strong>.
+						</p>
+					</div>
+					<br>
+					<div class='panel panel-primary'>
+						<div class='panel-heading'>IT-ASSOCIATION</div>
+					</div>
+					<div>
+						<img src="./imgs/ita.png" class='img-circle work_icons pull-left'>
+						<p style='margin-left:60px'>
+							Was an active member of <strong>IT Association, department level student run association</strong>, from October 2012 to May 2015 and also held the position of <strong>Committee Head of the Technical Team</strong> of the association.
+						</p>
+					</div>
+				</div>
+				<div class='col-md-1'></div>
+			</div>
+			<br><br><br><br><br>
+			<div class='row' id='academics'>
+				<div class='page-header'>
+					<h1>My Academics</h1>
+				</div>
+				<div class='col-md-1'></div>
+				<div class='col-md-10'>
+				<div class='panel panel-primary'>
+					<div class='panel-heading'>
+						<div class='panel-title'>
+							<h3 class='panel-title'>Academics</h3>
+						</div>
+					</div>
+					<div class='panel-body'>
+						<div role="tabpanel">
+							<ul class="nav nav-tabs" role="tablist">
+								<li role="presentation" class="active">
+									<a href="#college" aria-controls="college" role="tab" data-toggle="tab">Qualifications</a>
+								</li>
+								<li>
+									<a href="#projects" aria-controls="projects" role="tab" data-toggle="tab">Projects</a>
+								</li>								
+							</ul>
+							<div class='tab-content'>
+								<div role="tabpanel" class="tab-pane active" id="college">
+									<table class='table table-striped table-responsive'>
+										<tr>
+											<th>Degree:</th>
+											<td>B.Tech <em>(pursuing)</em></td>
+										</tr>	
+										<tr>
+											<th>Specialisation</th>
+											<td>Information Technology</td>
+										</tr>				
+										<tr>
+											<th>University Affiliation</th>
+											<td>SRM University</td>
+										</tr>
+										<tr>
+											<th>Place</th>
+											<td>Chennai,Tamil Nadu</td>
+										</tr>
+										<tr>
+											<th>Year of Passing</th>
+											<td>2016 (tentative)</td>
+										</tr>
+										<tr>
+											<th>Schooling (Class XII)</th>
+											<td>
+												ST. Paul Sr. Sec. Co-Ed School (CBSE), Bhopal, Madhya Pradesh
+											</td>
+										</tr>
+									</table>
+								</div>
+								<div role="tabpanel" class='tab-pane' id="projects">
+									<div class='panel-group' id='accordion' role='tablist' aria-multiselectable="true">
+										<div class='panel panel-default'>
+											<div class='panel-heading' role="tab" id="project1">
+												<h3 class='panel-title'>
+													<a href="#projectCollapse1" data-toggle="collapse" data-parent="#accordion" aria-expanded='true' aria-controls="projectCollapse1">
+														Research on Drag n Drop Captcha
+													</a>
+												</h3>
+											</div>
+											<div id='projectCollapse1' class='panel-collapse collapse in' role='tabpanel' aria-labelledby='project1'>
+												<div class='panel-body'>
+													Doing a Research on Drag 'n Drop Captcha as Research Incubator Program of college under the research guide Dr. Harish Ramani
+												</div>
+											</div>
+										</div>
+										<div class='panel panel-default'>
+											<div class='panel-heading' role='tab' id='project2'>
+												<h3 class='panel-title'>
+													<a href="#projectCollapse2" data-toggle="collapse" data-parent="#accordion" aria-expanded="true" aria-controls="projectCollapse2">
+														PHP based Registration Portal
+													</a>
+												</h3>
+											</div>
+											<div id='projectCollapse2' class='paenl-collapse collapse' role='tabpanel' aria-labelledby='project2'>
+												<div class='panel-body'>
+													Has worked upon the development of PHP based Enterprise level Registration portal for Aaruush'14, a techno-management fest of SRM University,Chennai. The application involved various types of registration and their report generation. The application was built using HTML,CSS and Ajax at the front end where as at the backend it used PHP and MySql for handling database, the application ran on XAMPP on Linux Ubuntu and Windows Server 2008 environment.
+												</div>
+											</div>
+										</div>
+										<div class='panel panel-default'>
+											<div class='panel-heading' role='tab' id='project3'>
+												<h3 class='panel-title'>
+													<a href="#projectCollapse3" data-toggle="collapse" data-parent="#accordion" aria-expanded="true">Techno-management Fest Website</a>
+												</h3>
+											</div>
+											<div id='projectCollapse3' class='panel-collapse collapse' role='tabpanel' aria-labelledby='porject3'>
+												<div class='panel-body'>
+													Has worked with the team which was involved in the design and development of website of Aaruush'14 a techno-management fest of SRM University, the website involved use of HTML, CSS3, javascript, jQuery and various other libraries of javascript
+												</div>
+											</div>
+										</div>
+										<div class='panel panel-default'>
+											<div class='panel-heading' role='tab' id='project4'>
+												<h3 class='panel-title'>
+													<a href="#projectCollapse4" data-toggle="collapse" data-parent="#accordion" aria-expanded="true">PHP based Interface for Android App</a>
+												</h3>
+											</div>
+											<div id='projectCollapse4' class='panel-collapse collapse' role='tabpanel' aria-labelledby='project4'>
+												<div class='panel-body'>
+													Has developed a PHP interface which was later used in surprise test android Application developed by SRM Aakash Centre working under IIT Mumbai Aakash Tablet Project
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				</div>
+				<div class='col-md-1'></div>
+			</div>
+			<div class='row' id='skills'>
+				<div class='page-header'>
+					<h1>My Skills</h1>
+				</div>
+				<div class='col-md-1'></div>
+				<div class='col-md-10'>
+					<div class='well'>
+						<p>C/C++</p>
+						<div class="progress">
+							<div class='progress-bar' role='progressbar' aria-valuenow="55" aria-valuemax="100" style='width:55%;'><span class='pull-left' style='margin-left:5px;'><strong>55%</strong></span></div>
+						</div>
+						<p>HTML/CSS</p>
+						<div class='progress'>
+							<div class='progress-bar' role='progressbar' aria-valuenow="65" aria-valuemax="100" style='width:65%;'><span class='pull-left' style='margin-left:5px;'><strong>65%</strong></span></div>
+						</div>
+						<p>JavaScript</p>
+						<div class='progress'>
+							<div class='progress-bar' role='progressbar' aria-valuenow="40" aria-valuemax="100" style='width:40%;'><span class='pull-left' style='margin-left:5px;'><strong>40%</strong></span></div>
+						</div>
+						<p>PHP/MySql</p>
+						<div class='progress'>
+							<div class='progress-bar' role='progressbar' aria-valuenow="70" aria-valuemax="100" style='width:75%;'><span class='pull-left' style='margin-left:5px;'><strong>75%</strong></span></div>
+						</div>
+						<p>Networking</p>
+						<div class='progress'>
+							<div class='progress-bar' role='progressbar' aria-valuenow="20" aria-valuemax="100" style='width:20%;'><span class='pull-left' style='margin-left:5px'><strong>20%</strong></span></div>
+						</div>
+						<p>Version Control System (GIT)</p>
+						<div class='progress'>
+							<div class='progress-bar' role='progressvar' aria-valuenow="30" aria-valuemax="100" style='width:30%;'><span class='pull-left' style='margin-left:5px'><strong>30%</strong></span></div>
+						</div>
+						<p>Adobe Photoshop</p>
+						<div class='progress'>
+							<div class='progress-bar' role='progressvar' aria-valuenow="20" aria-valuemax="100" style='width:30%;'><span class='pull-left' style='margin-left:5px'><strong>20%</strong></span></div>
+						</div>
+					</div>
+				</div>
+				<div class='col-md-1'></div>
+			</div>
+			<div class='row' id='contact'>
+				<div class='page-header'>
+					<h1>Contact Me</h1>		
+				</div>
+				<div class='col-md-1'></div>
+				<div class='col-md-4'>
+						<h3>Say Hi To me</h3>
+						<form>
+							<div class='form-group'>
+								<input type='text' class='form-control' placeholder='Your Name' name='senderName'>
+							</div>
+							<div class='form-group'>
+								<input type='text' class='form-control' placeholder='Your Contact No.' name='senderContact' maxlength="10">
+							</div>
+							<div class='form-group'>
+								<input type='text' class='form-control' placeholder='Your E-Mail' name='senderEmail'>
+							</div>
+							<div class='form-group'>
+								<textarea class='form-control' placeholder='Your Message' name='senderMsg' rows="3"></textarea>
+							</div>
+							<div class='form-group'>
+								<input type='submit' value='SUBMIT' class='btn btn-primary pull-right' name='submitBtn'>
+							</div>
+						</form>
+				</div>
+				<div class='col-md-2'></div>
+				<div class='col-md-4'>
+					<h3>Get Me on Social Network</h3>
+						<a href="http://www.facebook.com/anshul.sahni.52" target="_blank">
+							<img src="./imgs/fb.png" class='social_icons img-circle' >				
+						</a>
+						<a href="https://www.twitter.com/AnshulSahni93" target="_blank">
+							<img src="./imgs/twitter.png" class='social_icons img-circle' >
+						</a>
+						<a href="https://plus.google.com/u/0/115757878632716494689/" target="_blank">
+							<img src="./imgs/gplus.png" class='social_icons img-circle'>
+						</a>
+						<a href="https://www.linkedin.com/pub/anshul-sahni/63/809/660" target="_blank">
+							<img src="./imgs/linkedin.png" class='social_icons img-circle'>				
+						</a>
+						<a href="https://www.github.com/anshulsahni" target="_blank">
+							<img src="./imgs/github.png" class='social_icons img-circle'>
+						</a>
+					</div>
+				<div class='col-md-1'></div>
+			</div>
+		</div>
+		<br><br><br><br><br>
+		<nav class='navbar navbar-inverse footer' style='margin-bottom:0px; border-radius:0px;'>
+			<div cass='container-fluid'>
+				<div class='nav-header'>
+						<p class='navbar-text'>&copy; Anshul Sahni</p>
+				</div>
+			</div>
+		</nav>
+
+	</body>
+</html>
