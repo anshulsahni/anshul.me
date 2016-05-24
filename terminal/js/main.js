58af38e3af456d5937042f48e8c1556e463afb8d
Anshul Sahni <anshul_sahni@rediffmail.com>
2016-05-25 03:37:16 +0530

    set new greeting text for the terminal

--- terminal/js/main.js
+++ terminal/js/main.js
@@ -1,3 +1,16 @@
+var welcomeFolks = [
+'__        __   _                            __        _ _        ',
+'\\ \\      / /__| | ___ ___  _ __ ___   ___  / _|  ___ | | | _____ ',
+' \\ \\ /\\ / / _ \\ |/ __/ _ \\| \'_ ` _  \\/ _ \\ | |_ / _ \\| | |/ / __|',
+'  \\ V  V /  __/ | (_| (_) | | | | | |  __/ |  _| (_) | |   <\\__ \\ ',
+'   \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___| |_|  \\___/|_|_|\\_\\___/'].join('\n')
+
+var greetingText = welcomeFolks + '\n\n' +
+'\tWelcome to anshul sahni\'s web profile....Seems a bit different??\n' +
+'\tWell type \'help\' to get the list of commands available to you' +
+'\n\n';
+
+
 $('#terminal').terminal(function(command, cmd) {
   cmd.echo('You typed ' + command)
 
@@ -5,6 +18,6 @@ $('#terminal').terminal(function(command, cmd) {
     cmd.clear();
   }
 },{
-  prompt: '#',
-  greetings: 'Welcome to Anshul\'s world. Type \'help\' to get the set of commands that can be run'
+  prompt: 'you@ansh\'sworld > ',
+  greetings: greetingText
 })
